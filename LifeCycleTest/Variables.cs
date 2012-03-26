fcceef757068dfed758b99fa4d4f8b5296a9e4ce
Pål Rønning <pronning@gmail.com>
2012-03-26 22:23:27 +0200

    new file in shelf

---
+++ LifeCycleTest/Variables.cs
@@ -0,0 +1,30 @@
+
+
+namespace LifeCycleTest
+{
+public class myfeature {
+// master
+private const string myconst = "s4";
+
+}
+
+
+    public static class Constants
+    {
+        public static readonly string[] Fasit = {
+                                             "PreInit", 
+                                             "Init", 
+                                             "InitComplete",
+                                             "PreLoad", 
+                                             "Load", 
+                                             "PostBacks",
+                                             "LoadComplete", 
+                                             "PreRender", 
+                                             "SaveStateComplete", 
+                                             "Render", 
+                                             "Unload"
+                                         };
+
+        public static string Alphabet = "abcdefghijklmnopqrstuvwxyzæøå";
+    }
+}
