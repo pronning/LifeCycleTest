c2687b6d0faa43b3aed06eb69921445b68436a80
pronning <pr@makingwaves.no>
2012-03-28 17:16:36 +0200

    open ssh

--- LifeCycleTest/Tuple.cs
+++ LifeCycleTest/Tuple.cs
@@ -5,6 +5,7 @@ namespace LifeCycleTest
     class Item<T,TS> : Tuple<T,TS>
     {
         // vs2010
+        // open ssh
         public Item(T item1, TS item2) : base(item1, item2)
         {
         }
